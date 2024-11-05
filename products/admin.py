64e9f8f362c93c8735d83f7fdc00a6e9ada93f1c
John Doherty <johndoherty1984@live.com>
2024-11-05 09:43:15 +0000

    Customise admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'discount_price',
+        'stock_quantity',
+        'is_active',
+        'size',
+        'rating',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = ('category', 'is_active')  # Filter options for category and active status
+    search_fields = ('name', 'sku')  # Enable search by name and SKU
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
