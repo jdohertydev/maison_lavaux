1ef51b88343ec0f0e5b2f998d01ac18699922bc6
John Doherty <johndoherty1984@live.com>
2024-11-12 13:51:57 +0000

    Register model in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ContactSubmission
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+class ContactSubmissionAdmin(SummernoteModelAdmin):
+    summernote_fields = ('message',)
+
+admin.site.register(ContactSubmission, ContactSubmissionAdmin)
