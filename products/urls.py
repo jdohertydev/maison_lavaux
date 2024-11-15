aaeaa13d2849528e59b0b20515dd0d3d98767ad5
John Doherty <johndoherty1984@live.com>
2024-11-15 13:12:05 +0000

    Update review section. Add add_product.html, views and URLs

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:product_id>/reviews/<int:review_id>/edit/', views.edit_review, name='edit_review'),
     path('<int:product_id>/reviews/<int:review_id>/delete/', views.delete_review, name='delete_review'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
