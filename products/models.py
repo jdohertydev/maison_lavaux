cbd18f1d188fe0099cf74051fa5a3f8270594ac6
John Doherty <johndoherty1984@live.com>
2024-11-17 15:46:44 +0000

    Add unique constraint to SKU field in Product model

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     ]
 
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
     name = models.CharField(max_length=254)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')  # Gender field
     description = models.TextField()
