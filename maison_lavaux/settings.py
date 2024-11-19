f84a5e8ebd2aac42dd434b70f08b7b3db8b4a737
John Doherty <johndoherty1984@live.com>
2024-11-19 11:13:59 +0000

    Remove secret key and set debug

--- maison_lavaux/settings.py
+++ maison_lavaux/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=bzdt4cb$o=xe2cmgjj31(h!a*e9j%e^m(%6m13ba)w@r*+s1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['.herokuapp.com','8000-jdohertydev-maisonlavau-36c6hktzgpz.ws.codeinstitute-ide.net']
 
