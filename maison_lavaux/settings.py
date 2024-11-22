3ab709f8366c03efe323c0b564d49137a1e923d6
John Doherty <johndoherty1984@live.com>
2024-11-22 11:25:34 +0000

    Move setting variable

--- maison_lavaux/settings.py
+++ maison_lavaux/settings.py
@@ -241,8 +241,6 @@ STRIPE_CURRENCY = "usd"
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 
 # Email server configuration
 
