ce637550daee14f0f9911a18a1df1d57429a53cf
John Doherty <johndoherty1984@live.com>
2024-11-25 18:18:20 +0000

    Add PEP 8-compliant docstrings to signals for updating order totals

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,16 +3,18 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem update/create
+    Signal to update the order total when a line item is created or updated.
     """
     instance.order.update_total()
+
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update order total on lineitem delete
+    Signal to update the order total when a line item is deleted.
     """
     instance.order.update_total()
