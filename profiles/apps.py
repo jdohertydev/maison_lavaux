25de13f54951798b6e23037186ebe1b857bcf137
John Doherty <johndoherty1984@live.com>
2024-11-25 18:34:48 +0000

    Add PEP 8-compliant docstring to ProfilesConfig class

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """
+    Configuration for the Profiles app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
