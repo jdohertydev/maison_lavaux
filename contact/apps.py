cd6936964bc69d6cd0881e684f237d4619897c7f
John Doherty <johndoherty1984@live.com>
2024-11-25 18:21:55 +0000

    Add PEP 8-compliant docstring to Contact app configuration

--- contact/apps.py
+++ contact/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ContactConfig(AppConfig):
+    """
+    Configuration class for the Contact app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
