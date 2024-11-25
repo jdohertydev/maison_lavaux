1d6b9a05d4f7116cceed4e7f717068eb87b0ee54
John Doherty <johndoherty1984@live.com>
2024-11-25 18:02:22 +0000

    Add PEP 8-compliant docstring to AnalyticsConfig class

--- analytics/apps.py
+++ analytics/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AnalyticsConfig(AppConfig):
+    """
+    Configuration for the Analytics app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analytics'
