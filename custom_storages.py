859fed90f4738a006c55921848dbdb3ccdb4f17c
John Doherty <johndoherty1984@live.com>
2024-11-25 18:56:21 +0000

    Add custom S3 storage backends for static and media files with PEP 8 style docstrings

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
+    """
+    Custom storage backend for handling static files on Amazon S3.
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Custom storage backend for handling media files on Amazon S3.
+    """
     location = settings.MEDIAFILES_LOCATION
