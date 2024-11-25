b21a8061d5f4ea30c092fcc68f75fa72be377723
John Doherty <johndoherty1984@live.com>
2024-11-25 18:31:02 +0000

    Add PEP 8-compliant docstring to ProductsConfig with signals import

--- products/apps.py
+++ products/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
 
     def ready(self):
+        """Import signals to activate custom model events."""
         import products.signals  # Ensure signals are imported
