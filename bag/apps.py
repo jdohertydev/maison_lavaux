a8cfb19cd559a5ea4dea6defac585e8f86a6ced2
John Doherty <johndoherty1984@live.com>
2024-11-25 18:05:11 +0000

    Add PEP 8-compliant docstring to BagConfig

--- bag/apps.py
+++ bag/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class BagConfig(AppConfig):
+    """
+    Configuration for the Bag application.
+    Handles settings and initialization for the Bag app.
+    """
     name = 'bag'
